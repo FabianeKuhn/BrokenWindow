04e1424bcf18a47c108fd52cbccd36940c9cd26a
FabianeKuhn <FabianeKuhn@users.noreply.github.com>
2019-05-24 18:47:07 -0300

    Update BadTrimino.java

--- BadTrimino.java
+++ BadTrimino.java
@@ -1,3 +1,4 @@
+//Grupo: Fabiane, Rafael Alves, Savanna e Franciele
 // A implementação utiliza divisão e conquista para preencher a tabela. Inicialmente a tabela é dividida em
 //.. quatro quadrantes, então é visto em qual quadrante o buraco se encontra. O quadrante com o buraco é o
 //.. primeiro a ser preenchido. Após o preenchimento do quadrante com o buraco, é inserida uma peça no meio
